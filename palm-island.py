0a694ac0e47b54906b431b6c2e648697fa864905
Davi Diorio Mendes <ddioriomendes@gmail.com>
2025-04-15 18:30:25 -0300

    Rework on actions and costs

--- palm-island.py
+++ palm-island.py
@@ -5,38 +5,52 @@ This code is intended to given a randon deck shuffle of Palm Island
 generate all the possible games and its points.
 '''
 
-KIND_WOOD = "WOOD"
-KIND_FISH = "FISH"
+
+ACT_LVL_UP = 'ACT_LVL_UP'
+ACT_TO_LVL1 = 'ACT_TO_LVL1'
+ACT_TO_LVL2 = 'ACT_TO_LVL2'
+ACT_TO_LVL2 = 'ACT_TO_LVL3'
+ACT_TO_LVL2 = 'ACT_TO_LVL4'
+ACT_TO_HAND = 'ACT_TO_HAND'
 
 
 class Resource():
 
-    def __init__(self, amount, kind):
-        self.amount = amount
-        self.kind = kind
+    def __init__(self, wood, fish, stone):
+        self.wood = wood
+        self.fish = fish
+        self.stone = stone
 
 
 class Action():
 
-    def __init__(self, card, cost):
-        self.card = card
+    def __init__(self, cost, action):
         self.cost = cost
-
-    def execute(self):
-        pass
+        self.action
 
 
 class Card():
 
-    def __init__(self, id, name, actions, victory_points):
+    def __init__(self, id, name, levels):
         self.id = id
         self.name = name
-        self.level = 0
-        self.in_hand = False
-        self.victory_points = victory_points
+        self.levels = levels
+        self.level_idx = 0
 
-    def _set_actions(self, actions):
+
+class CardLevel():
+
+    def __init__(self, value, actions, vp):
+        self.value = value
         self.actions = actions
+        self.vp = vp
+
+
+class Player():
+
+    def __init__(self, deck):
+        self.deck = deck
+
 
 
 class PalmIsland():

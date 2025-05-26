f98197e659951e8710306d7a1df4ab0190ba6988
Davi Diorio Mendes <ddioriomendes@gmail.com>
2025-05-26 18:04:51 -0300

    Added canoaria and madeireira cards

--- palm-island.py
+++ palm-island.py
@@ -44,10 +44,10 @@ class Card():
 
 class CardLevel():
 
-    def __init__(self, resources, actions, vp):
+    def __init__(self, vp, resources, actions):
+        self.vp = vp
         self.resources = resources
         self.actions = actions
-        self.vp = vp
 
 
 class Player():
@@ -89,20 +89,41 @@ class PalmIslandDeck():
 def constructDeckList():
     return [
         Card(1, "Canoaria", [
-            CardLevel(Resource(fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1), ACT_TO_LVL1), Action(fish = 1, ACT_TO_LVL2)]),
-            CardLevel(Resource(fish = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1, wood = 1), ACT_TO_LVL3)]),
-            CardLevel(Resource(wood = 1, fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL3)])
+            CardLevel(0, Resource(fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1), ACT_TO_LVL1), Action(fish = 1, ACT_TO_LVL2)]),
+            CardLevel(0, Resource(fish = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1, wood = 1), ACT_TO_LVL3)]),
+            CardLevel(0, Resource(wood = 1, fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL3)]),
+            CardLevel(0, Resource(wood = 1, fish = 2), [Action(Resource(), ACT_TO_HAND)
         ]),
         Card(2, "Canoaria", [
-            CardLevel(Resource(fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1), ACT_TO_LVL1), Action(fish = 1, ACT_TO_LVL2)]),
-            CardLevel(Resource(fish = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1, wood = 1), ACT_TO_LVL3)]),
-            CardLevel(Resource(wood = 1, fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL3)])
+            CardLevel(0, Resource(fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1), ACT_TO_LVL1), Action(fish = 1, ACT_TO_LVL2)]),
+            CardLevel(0, Resource(fish = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1, wood = 1), ACT_TO_LVL3)]),
+            CardLevel(0, Resource(wood = 1, fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL3)]),
+            CardLevel(0, Resource(wood = 1, fish = 2), [Action(Resource(), ACT_TO_HAND)
         ]),
         Card(3, "Canoaria", [
-            CardLevel(Resource(fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1), ACT_TO_LVL1), Action(fish = 1, ACT_TO_LVL2)]),
-            CardLevel(Resource(fish = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1, wood = 1), ACT_TO_LVL3)]),
-            CardLevel(Resource(wood = 1, fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL3)])
+            CardLevel(0, Resource(fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1), ACT_TO_LVL1), Action(fish = 1, ACT_TO_LVL2)]),
+            CardLevel(0, Resource(fish = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(fish = 1, wood = 1), ACT_TO_LVL3)]),
+            CardLevel(0, Resource(wood = 1, fish = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL3)]),
+            CardLevel(0, Resource(wood = 1, fish = 2), [Action(Resource(), ACT_TO_HAND)
         ]),
+        Card(4, "Madeireira", [
+            CardLevel(0, Resource(wood = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL1)]),
+            CardLevel(1, Resource(wood = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL2)]),
+            CardLevel(2, Resource(wood = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 2, stone = 2), ACT_TO_LVL3)]),
+            CardLevel(5, Resource(), [])
+        ]),
+        Card(5, "Madeireira", [
+            CardLevel(0, Resource(wood = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL1)]),
+            CardLevel(1, Resource(wood = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL2)]),
+            CardLevel(2, Resource(wood = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 2, stone = 2), ACT_TO_LVL3)]),
+            CardLevel(5, Resource(), [])
+        ]),
+        Card(6, "Madeireira", [
+            CardLevel(0, Resource(wood = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL1)]),
+            CardLevel(1, Resource(wood = 1), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 1, fish = 1), ACT_TO_LVL2)]),
+            CardLevel(2, Resource(wood = 2), [Action(Resource(), ACT_TO_HAND), Action(Resource(wood = 2, stone = 2), ACT_TO_LVL3)]),
+            CardLevel(5, Resource(), [])
+        ])
     ]
 
 
